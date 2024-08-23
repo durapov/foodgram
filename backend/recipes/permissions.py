21b83de65752a0dd730ec32bcf4735c4d2ce54f5
durapov <ddurapov@gmail.com>
2024-08-23 03:45:46 +0300

    fix back after review v3 flake

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -6,8 +6,8 @@ class CustomPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in ('PATCH', 'DELETE'):
             return obj.author == request.user
-        if (request.method in ('PATCH', 'DELETE') and
-                request.user.is_authenticated):
+        if (request.method in ('PATCH', 'DELETE')
+                and request.user.is_authenticated):
             return request.user.is_admin
         if request.method in SAFE_METHODS:
             return True
